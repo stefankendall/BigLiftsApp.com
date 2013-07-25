043ae61d9543831abdbe7ad48a30005952d8f559
Stefan Kendall <stefankendall@gmail.com>
2013-07-24 21:03:40 -0400

    Switching rails with sinatra. It's a marketing page.

---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+set :public_folder, 'public'
+get "/" do
+  redirect '/index.html'
+end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-BigliftsappCom::Application.config.session_store :cookie_store, key: '_bigliftsapp-com_session'
